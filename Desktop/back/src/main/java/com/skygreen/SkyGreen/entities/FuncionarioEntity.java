f81b658712926a037635cb4e4b989908b8e2a1f6
Gustavo Luís França Gonçalves <gustavo.franca@dc.oscar>
2024-09-10 19:00:31 -0300

    Implementação do UserDetails no funcionarioEntity

--- Desktop/back/src/main/java/com/skygreen/SkyGreen/entities/FuncionarioEntity.java
+++ Desktop/back/src/main/java/com/skygreen/SkyGreen/entities/FuncionarioEntity.java
@@ -1,9 +1,11 @@
 package com.skygreen.SkyGreen.entities;
 
-import java.io.Serializable;
+import java.util.Collection;
 
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -17,7 +19,7 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "FUNCIONARIO")
-public class FuncionarioEntity implements Serializable {
+public class FuncionarioEntity implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -35,7 +37,41 @@ public class FuncionarioEntity implements Serializable {
     private String nome;
     @CPF
     private String cpf;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        return null;
+    }
+
+    @Override
+    public String getUsername(){
+        return cpf;
+    }
     
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+     }
+     
+     @Override
+     public boolean isEnabled() {
+        return true;
+     }
+    
+     @Override
+     public boolean isAccountNonLocked() {
+        return true;
+     }
 
 
 }
