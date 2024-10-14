e1b302e49881311ab483be0799a368fde79ebd92
joaosalesvn <jvsalesnascimento11@gmail.com>
2024-10-14 20:50:06 -0300

    feat: Implementa CRUD ordem de produção

--- Desktop/back/src/main/java/com/skygreen/SkyGreen/entities/ProducaoEntity.java
+++ Desktop/back/src/main/java/com/skygreen/SkyGreen/entities/ProducaoEntity.java
@@ -1,11 +1,14 @@
 package com.skygreen.SkyGreen.entities;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -25,4 +28,17 @@ public class ProducaoEntity implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Integer;
 
+    private Integer sementeId; 
+    private Integer sementeQuantidade; 
+
+    private Integer tempoCultivo;
+    private Date dataInicio;
+
+    private String fotoSemente;
+
+    private Boolean ativo;
+
+    @OneToOne
+    @JoinColumn(name = "prateleira_id")
+    private PrateleiraEntity prateleira;
 }
