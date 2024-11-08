7b4ccf6d9e1680871b494e4a0fed04ccc48cbf87
Cauane Oliveira <cauaneoliveira321@gmail.com>
2024-11-08 19:35:30 -0300

    tela home finalizada

--- Desktop/Codigo Front (Python)/tela_home.py
+++ Desktop/Codigo Front (Python)/tela_home.py
@@ -14,43 +14,59 @@ class telaHome:
         self.frame.pack(fill="both", expand=True)
         
         # Card 1
-        card2 = ctk.CTkFrame(self.frame, width=350, height=440, corner_radius=10)
-        card2.place(x=45, y=15)
+        card1 = ctk.CTkFrame(self.frame, width=350, height=440, corner_radius=10)
+        card1.place(x=45, y=15)
 
-        self.listaSementes_frame = ctk.CTkScrollableFrame(card2, width=350, height=400)
+        self.listaSementes_frame = ctk.CTkScrollableFrame(card1, width=350, height=400)
         self.listaSementes_frame.pack( pady=10, padx=10)
 
         self.lista_sementes()
 
-        btn_saibamais2 = ctk.CTkButton(card2, text="Insumos", width=392,fg_color=Style.color('fg'),hover_color=Style.color('hover'),command=self.show_sementes)
+        btn_saibamais2 = ctk.CTkButton(card1, text="Insumos", width=392,fg_color=Style.color('fg'),hover_color=Style.color('hover'),command=self.show_sementes)
         btn_saibamais2.place(x=0, y=405)
 
         # Card 2
-        card3 = ctk.CTkFrame(self.frame, width=359, height=440, corner_radius=10)
-        card3.place(x=500, y=15)
+        card2 = ctk.CTkFrame(self.frame, width=359, height=440, corner_radius=10)
+        card2.place(x=500, y=15)
 
-        self.listaPedidos_frame = ctk.CTkScrollableFrame(card3, width=350, height=400)
+        self.listaPedidos_frame = ctk.CTkScrollableFrame(card2, width=350, height=400)
         self.listaPedidos_frame.pack(pady=10, padx=10)
 
         self.lista_pedidos()
 
-        btn_saibamais3 = ctk.CTkButton(card3, text="Pedidos", width=392,fg_color=Style.color('fg'),hover_color=Style.color('hover'),command=self.show_pedidos)
+        btn_saibamais3 = ctk.CTkButton(card2, text="Pedidos", width=392,fg_color=Style.color('fg'),hover_color=Style.color('hover'),command=self.show_pedidos)
         btn_saibamais3.place(x=0, y=405)
 
-        # Gráfico de Produção Geral
-        prod_valor = [46, 35, 19]
-        prod_desc = ['Plantio', 'Pedidos', 'Colheita']
+        #graficos
+        
+        pedidosVenda = Access.listarpedidosVenda()
+        pedidosCompra = Access.listarpedidosCompra()
+        producao = Access.listarProducao()
+        
+        qtd_venda = sum(pedido['qtd'] for pedido in pedidosVenda)
+        qtd_compra = sum(pedido['qtd'] for pedido in pedidosCompra)
+        qtd_producao = sum(producao_item['qtd'] for producao_item in producao)
+
+        prod_valor = [qtd_venda, qtd_compra, qtd_producao]
+        prod_desc = ['Pedidos Venda', 'Pedidos Compra', 'Em Produção']
         
         canvas = Style.criar_grafico_circular(self.frame,prod_valor, prod_desc, "Produção Geral")
         canvas.draw()
         canvas.get_tk_widget().place(x=895, y=15, height=240, width=380)
         canvas.get_tk_widget().config(bg=Style.color('bg'), highlightthickness=0)
 
-        # Gráfico de Sementes
-        se_valor = [100, 46, 35]
-        se_desc = ['Alface', 'Tomate', 'Milho']
         
-        canvas = Style.criar_grafico_circular(self.frame,se_valor, se_desc, "Sementes")
+        #===
+        
+        estoque = Access.listarEstoque()
+        
+        se_valor = [item['qtd'] for item in estoque]
+        se_desc = [item['nome_semente'] for item in estoque]
+
+        quantidade_total = sum(se_valor)
+        se_valor = [(valor / quantidade_total) * 100 for valor in se_valor]
+        
+        canvas = Style.criar_grafico_circular(self.frame,se_valor, se_desc, "Estoque")
         canvas.draw()
         canvas.get_tk_widget().place(x=895, y=250, height=240, width=380)
         canvas.get_tk_widget().config(bg=Style.color('bg'), highlightthickness=0)
