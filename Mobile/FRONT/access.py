68019dd019cbab6c9d855d659496f3be6cdddd74
Cauane Oliveira <cauaneoliveira321@gmail.com>
2024-11-20 18:57:20 -0300

    tela de monitoramento conectado com api
    falta ver se esta funcionando

--- Mobile/FRONT/access.py
+++ Mobile/FRONT/access.py
@@ -1,13 +1,29 @@
 # access.py
 import flet as ft
 import requests
+from datetime import datetime, timezone
+from dateutil import parser
 
 API_BASE = "http://localhost:8080/skygreen"
 
 api_url = "http://localhost:8080/skygreen/auth/login"
 api_listarPedidosVenda = f"{API_BASE}/vendas/"
+api_listarPrateleira = f"{API_BASE}/prateleira/"
+api_listarSementes = f"{API_BASE}/sementes/"
+
+class Funcoes:
+
+    def calcular_dias_restantes(data_inicio, tempo_cultivo):
+        data_inicio = parser.isoparse(data_inicio)  # Lida com o formato ISO 8601
+        data_atual = datetime.now(timezone.utc)
+        dias_passados = (data_atual - data_inicio).days  # Calcula a diferença em dias
+        dias_restantes = tempo_cultivo - dias_passados  # Subtrai dos dias totais de cultivo
+
+        return dias_restantes if dias_restantes > 0 else 0
 
 class Access:
+    token = None
+    userId = None
     
     def __init__(self, token):
         self.token = token
@@ -24,18 +40,14 @@ class Access:
         
         try:
             response = requests.post(api_url, json=payload, headers=headers)
-            #print(f"Status da resposta: {response.status_code}")
-            #print(f"Conteúdo da resposta: {response.text}")
             
             if response.status_code == 200:
                 data = response.json()
-                token = data.get("token")
-                userId = data.get("userId")
+                Access.token = data.get("token")
+                Access.userId = data.get("userId")
+
                 
-                #print(f"Token recebido: {token}")
-                #print(f"ID do usuário: {userId}")
-                
-                return token, userId, "Login realizado com sucesso", ft.colors.GREEN
+                return Access.token, Access.userId, "Login realizado com sucesso", ft.colors.GREEN
             else:
                 return None, None, "CPF ou senha inválidos.", ft.colors.RED
         
@@ -52,3 +64,59 @@ class Access:
             return response.json()  # Retorna a lista de pedidos
         else:
             response.raise_for_status()  # Levanta erro se status for diferente de 200
+    
+    def listarSementes():
+        headers = {"Authorization": f"Bearer {Access.token}"}
+
+        response = requests.get(api_listarSementes, headers=headers)
+        
+        if response.status_code == 200:
+            sementes_api = response.json()
+            sementes = []
+            for i in sementes_api:
+                sementes.append({
+                    "id": i.get('sementeId'),
+                    "nome": i.get('nome'),
+                    "descricao": i.get('descricao')
+                })
+            return sementes 
+        else:
+            print('Falha ao listar sementes:',  response.text)
+    
+        
+    def listarPrateleira():
+        headers = {"Authorization": f"Bearer {Access.token}"}
+        response = requests.get(api_listarPrateleira, headers=headers)
+        
+        if response.status_code == 200:
+
+            prateleira_api = response.json()
+            list_sementes = {semente['id']: semente['nome'] for semente in Access.listarSementes()}
+            prateleira = []
+            
+            for prat in prateleira_api:
+                producao = prat.get('producao')
+                statusprat = prat.get('disponivel')
+                
+                if producao and 'dataInicio' in producao and 'tempoCultivo' in producao:
+                    diasRestantes = Funcoes.calcular_dias_restantes(producao['dataInicio'], producao['tempoCultivo'])
+                    tempoCultivo = producao.get('tempoCultivo')
+                else:
+                    diasRestantes = None
+                    tempoCultivo = None
+                
+                prateleira.append({
+                    "id": prat.get('prateleira_id'),
+                    "disponivel": "Produção" if not statusprat else "Disponivel",
+                    "producao": {
+                        "idseed": producao.get('sementeId'),
+                        "nome_semente": list_sementes.get(producao.get('sementeId')),
+                        "dataInicio": producao.get('dataInicio'),
+                        "status": "Ativo" if producao.get('ativo') else "Inativo",
+                        "graf_valor": [tempoCultivo,diasRestantes]
+                    } if not statusprat and producao else None
+                })
+
+            return prateleira
+        else:
+            return False
--- Mobile/FRONT/tela_monitoramento.py
+++ Mobile/FRONT/tela_monitoramento.py
@@ -1,5 +1,7 @@
 import flet as ft
+
 from menu import Menu
+from access import Access
 
 class telaMonitoramento(ft.UserControl):
     def __init__(self, page):
@@ -7,63 +9,38 @@ class telaMonitoramento(ft.UserControl):
         self.page = page
         self.dados = self.obter_dados()
 
-        # Adicionando o menu à tela
         self.menu = Menu(self.page, active_screen="home")
-        self.page.add(self.menu.build())  # Adiciona o menu na tela
+        self.page.add(self.menu.build())
 
-        # Configurações da página
         self.page.title = "Prateleiras - SkyGreens"
         self.page.padding = 20
         self.page.scroll = "adaptive"
 
-        # Menu suspenso para filtro
-        self.filtro_var = ft.Dropdown(
-            label="Filtrar por status:",
-            options=[
-                ft.dropdown.Option("Disponível"),
-                ft.dropdown.Option("Produção")
-            ],
-            value="Produção",
-            on_change=self.aplicar_filtro,
-        )
-
-        # Espaço para exibição dos cartões
-        self.cards_container = ft.Row(
-            spacing=10,
-            wrap=True,
-            alignment=ft.MainAxisAlignment.START
-        )
+        self.cards_container = ft.Row(spacing=10,wrap=True,alignment=ft.MainAxisAlignment.START)
 
         # Montar layout inicial
-        self.page.add(self.filtro_var, self.cards_container)
-        self.exibir_prateleiras("Produção")
+        self.page.add(self.cards_container)
+        self.exibir_prateleiras()
 
     def obter_dados(self):
-        # Mock de dados, substitua pelo seu método real de acesso aos dados
-        return [
-            {"id": 1, "disponivel": "Produção", "producao": {"idseed": "123", "nome_semente": "Alface", "graf_valor": [20, 80]}},
-            {"id": 2, "disponivel": "Disponível", "producao": None},
-            {"id": 3, "disponivel": "Produção", "producao": {"idseed": "124", "nome_semente": "Tomate", "graf_valor": [40, 60]}},
-        ]
+        result = Access.listarPrateleira()
+        return result
 
-    def aplicar_filtro(self, e):
-        self.exibir_prateleiras(self.filtro_var.value)
-
-    def exibir_prateleiras(self, filtro):
+    def exibir_prateleiras(self):
         # Limpar os cartões existentes
         self.cards_container.controls.clear()
 
-        # Criar novos cartões com base no filtro
+        # Criar novos cartões apenas para as prateleiras com status "Produção"
         for i in self.dados:
-            if i['disponivel'] != filtro:
+            if i['disponivel'] != "Produção":  # Exibe apenas as prateleiras "Produção"
                 continue
 
             card = ft.Container(
                 width=280,
                 height=200,
                 padding=10,
-                bgcolor=ft.colors.LIGHT_GREEN if i['disponivel'] == "Disponível" else ft.colors.LIGHT_BLUE,
-                border=ft.border.all(2, ft.colors.GREEN if i['disponivel'] == "Disponível" else ft.colors.BLUE),
+                bgcolor=ft.colors.LIGHT_GREEN,  # Cor para "Produção"
+                border=ft.border.all(2, ft.colors.GREEN),  # Cor de borda para "Produção"
                 border_radius=10,
                 content=ft.Column(
                     [
